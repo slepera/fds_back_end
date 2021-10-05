b7f53261c8e41f3241b9d048fd9d03258af85519
Emanuele <dangelo.emanuele.94@gmail.com>
2021-10-05 17:39:22 +0200

    modified log in case of failed login

--- user-service/src/main/java/com/services/UserService.java
+++ user-service/src/main/java/com/services/UserService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -49,10 +50,10 @@ public class UserService {
     public User authenticate(User user) {
         User user1 = this.repository.findByEmail(user.getEmail());
         HttpEntity<Log> request;
-        if(user1!=null){
+        if(user1!=null && user1.getPassword().equals(BCrypt.hashpw(user.getPassword(), user1.getSalt()))){
             this.logService.sendLog("Info", "UserService",  "User " + user.getEmail() + " logged in.");
         }else{
-            this.logService.sendLog("Info", "UserService",  "User authentication failed  for " + user.getEmail());
+            this.logService.sendLog("Warning", "UserService",  "User authentication failed  for " + user.getEmail());
         }
         return user1;
     }
